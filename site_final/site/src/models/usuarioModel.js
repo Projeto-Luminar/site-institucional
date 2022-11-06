d8f42151dae158035ebcc52cf5c86d84f6bbd13c
Ju <110927788+Eujuba@users.noreply.github.com>
2022-11-06 20:17:19 -0300

    Add files via upload

--- site_final/site/src/models/usuarioModel.js
+++ site_final/site/src/models/usuarioModel.js
@@ -19,14 +19,23 @@ function entrar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nome, email, senha, cpf) {
+function cadastrar(nome, sobrenome, telefone, cpf, email, senha, nome_empresa, cnpj, rua, cep, numero, cidade, estado) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
+
+
+    // Aqui seguindo nosso formulário de cadastro, ele deve inserir em 3 tabelas (empresa, usuário e endereço)
+    // porém é muito confuso pois são 3 inserts de 1 vez, e não aprendemos sobre a api inteira
+
+    // tentei fazer da seguinte forma após a linha 37 :
+    // insert into empresa (cnpj, nome_empresa) values ('${cnpj}', '${nome_empresa}');
+    // insert into endereco (rua, cep, numero, cidade, estado) values ('${rua}', '${cep}', '${numero}', '${cidade}', '${estado}');
+
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha,cpf) VALUES ('${nome}', '${email}', '${senha}','${cpf}');
-    `;
+        INSERT INTO usuario (cpf, nome, email, senha) VALUES ('${cpf}', '${nome}', '${email}', '${senha}');        
+        `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
