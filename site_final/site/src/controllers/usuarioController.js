fb4c22c0b57a69b2ff9b73bb2224c07490cf0a6c
Ju <110927788+Eujuba@users.noreply.github.com>
2022-11-06 20:14:06 -0300

    Add files via upload

--- site_final/site/src/controllers/usuarioController.js
+++ site_final/site/src/controllers/usuarioController.js
@@ -60,24 +60,56 @@ function entrar(req, res) {
 
 }
 
+
+
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
+    var sobrenome = req.body.sobrenomeServer;
+    var telefone = req.body.telefoneServer;
+    var cpf = req.body.cpfServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var cpf = req.body.cpfServer;
+    var nome_empresa = req.body.nome_empresaServer;
+    var cnpj = req.body.cnpjServer;
+    var rua = req.body.ruaServer;
+    var cep = req.body.cepServer;
+    var numero = req.body.numeroServer;
+    var cidade = req.body.cidadeServer;
+    var estado = req.body.estadoServer;
+
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
+    } else if (sobrenome == undefined) {
+        res.status(400).send("Seu sobrenome está indefinido!");
+    } else if (telefone == undefined) {
+        res.status(400).send("Seu telefone está indefinido!");
+    } else if (cpf == undefined) {
+        res.status(400).send("Seu cpf está indefinido!");
+    } else if (email == undefined) {    
         res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
+    } else if (senha == undefined) {    
+        res.status(400).send("Sua senha está indefinida!");
+    } else if (nome_empresa == undefined) {
+        res.status(400).send("Seu nome da empresa está indefinido!");
+    } else if (cnpj == undefined) {
+        res.status(400).send("Seu cnpj está indefinido!");
+    } else if (rua == undefined) {
+        res.status(400).send("Sua rua está indefinida!");
+    } else if (cep == undefined) {
+        res.status(400).send("Seu cep está indefinido!");
+    } else if (numero == undefined) {
+        res.status(400).send("Seu número está indefinido!");
+    } else if (cidade == undefined) {
+        res.status(400).send("Sua cidade está indefinida!");
+    } else if (estado == undefined) {
+        res.status(400).send("Seu estado está indefinido!");
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, cpf)
+        usuarioModel.cadastrar(nome, sobrenome, telefone, cpf, email, senha, nome_empresa, cnpj, rua, cep, numero, cidade, estado)
             .then(
                 function (resultado) {
                     res.json(resultado);
