89ff5bb9e214a935be0ef00afe8fc9dd1a4ea46d
benichel <giovanna.santos@sptech.school>
2022-11-14 00:22:46 -0300

    validação da tela cadastro de novos funcionario e tela cadastrosala (js)

---
+++ site_final/site/public/js/cadastro_sala.js
@@ -0,0 +1,12 @@
+function cadastrar(){
+    var sala = input_nome_sala.value; 
+    var metro_sala = Number(input_metros_sala.value);
+    var andar_sala = Number(input_andar_sala.value);
+     
+    if(sala == "" || metro_sala == 0 || andar_sala == 0){
+     alert('Atenção! Todos os campos precisam estar preenchidos')
+    }else{
+     
+    }
+
+ }
